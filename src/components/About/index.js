9f3e2f6aa1d40d831748dfbbfd45b70f7ab7e154
Robert Augusta <RobAAugusta@gmail.com>
2022-02-15 12:25:08 -0500

    Working on some final touches

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,10 +8,7 @@ function About() {
         <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
         </div>
         <p>
-        Helvetica put a bird on it church-key Blue Bottle banjo bespoke brunch Etsy authentic Marfa quinoa typewriter plaid direct trade small batch wayfarers bicycle rights cliche craft beer gastropub single-origin coffee Godard Carles you probably haven't heard of them irony pickled kitsch synth sriracha gentrify literally heirloom blog Truffaut paleo scenester
-        </p>
-        <p>
-        Echo Park sriracha Pinterest vegan biodiesel 90's irony iPhone Kickstarter Carles crucifix kitsch narwhal dreamcatcher pickled trust fund selvage art party letterpress Tumblr post-ironic kogi Thundercats Tonx Brooklyn Pitchfork Odd Future authentic normcore freegan leggings Schlitz chambray organic tousled retro fap squid street art church-key fashion axe Wes Anderson bespoke whatever gentrify banh mi you probably haven't heard of them American Apparel next level pug ugh.
+        Garage Door Technician with 7+ years experience in diagnosing/troubleshooting, installation, repair and maintenance. Poccess a certificate of Completion from UNH Professional Development & Training and a comprehensive portfolio of projects to demostrate mastery of various topics.
         </p>
       </div>
     </section>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -6,34 +6,34 @@ function Portfolio() {
   // Replace links with deployed projects and GitHub repos
   const [projects] = useState([
     {
-      name: 'surf-report',
-      description: 'MERN Stack',
-      link: "https://github.com",
-      repo: "https://github.com"
+      name: 'Text-Editor',
+      description: 'Create notes or code snippets with or without an internet connection SO THAT I can reliably retrieve them for later use',
+      link: "https://blooming-fjord-22836.herokuapp.com/",
+      repo: "https://github.com/Offspringjunky90/Text-Editor"
     },
     {
-      name: 'pastel-puzzels',
-      description: 'MERN Stack',
-      link: "https://github.com",
-      repo: "https://github.com"
+      name: 'Social-Network-API',
+      description: 'An API for my social network that uses a NoSQL database SO THAT my website can handle large amounts of unstructured data',
+      link: "https://www.youtube.com/watch?v=cnfw_tClfTw",
+      repo: "https://github.com/Offspringjunky90/Social-Network-API"
     },
     {
-      name: 'run-buddy',
-      description: 'HTML/CSS',
-      link: "https://github.com",
-      repo: "https://github.com"
+      name: 'Day-Planner',
+      description: 'The Day-Planner calendar application allows a user to save events for each hour of the day.. This app runs in the browser and features dynamically updated HTML and CSS powered by jQuery.',
+      link: "https://offspringjunky90.github.io/Day-Planner/",
+      repo: "https://github.com/Offspringjunky90/Day-Planner"
     },
     {
-      name: 'led-wall',
-      description: 'Node/IoT',
-      link: "https://github.com",
-      repo: "https://github.com"
+      name: 'Team-Profile-Generator',
+      description: 'Generates a webpage that displays my teams basic info SO THAT I have quick access to their emails and GitHub profiles',
+      link: "https://www.youtube.com/embed/-Ak7IrCL6ac",
+      repo: "https://github.com/Offspringjunky90/Team-Profile-Generator"
     },
     {
-      name: 'calculator',
-      description: 'React/JavaScript/CSS',
-      link: "https://github.com",
-      repo: "https://github.com"
+      name: 'Weather-Dashboard',
+      description: 'Upon entering the city of your choice and clicking submit, you are able to instantly view the current weather along with the five day forecast.',
+      link: "https://offspringjunky90.github.io/Weather-Dashboard/",
+      repo: "https://github.com/Offspringjunky90/Weather-Dashboard"
     },
   ]);
 
