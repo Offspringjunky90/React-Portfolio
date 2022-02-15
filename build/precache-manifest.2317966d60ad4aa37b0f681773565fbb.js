d45afc3a5faed058617a131cb6f2598e445bf1d6
Robert Augusta <RobAAugusta@gmail.com>
2022-02-15 18:14:16 -0500

    Fixing Bugs

---
+++ build/precache-manifest.2317966d60ad4aa37b0f681773565fbb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b94da7d6f1a57fe9013c0e9edb263fc",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "68ceda44fac2d96597d7",
+    "url": "/React-Portfolio/static/css/main.08325502.chunk.css"
+  },
+  {
+    "revision": "ec7dea27f2d267470dff",
+    "url": "/React-Portfolio/static/js/2.102548e6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-Portfolio/static/js/2.102548e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68ceda44fac2d96597d7",
+    "url": "/React-Portfolio/static/js/main.924b2b7b.chunk.js"
+  },
+  {
+    "revision": "7c35ff808cd94c4ce20c",
+    "url": "/React-Portfolio/static/js/runtime-main.0ad08de7.js"
+  },
+  {
+    "revision": "a19f2b67a0540f784bf091db8e36c82a",
+    "url": "/React-Portfolio/static/media/calculator.a19f2b67.jpg"
+  },
+  {
+    "revision": "402a471b5aad190a33b649f1368f5de1",
+    "url": "/React-Portfolio/static/media/cover-image.402a471b.jpg"
+  },
+  {
+    "revision": "653c2b3d29f66a3c6e957518f3bd16f0",
+    "url": "/React-Portfolio/static/media/led-wall.653c2b3d.jpg"
+  },
+  {
+    "revision": "ed494730868bd902e0ee98f1df1877e2",
+    "url": "/React-Portfolio/static/media/pastel-puzzels.ed494730.jpg"
+  },
+  {
+    "revision": "938f8fe0fe2e5880b3ef528c5188a096",
+    "url": "/React-Portfolio/static/media/run-buddy.938f8fe0.jpg"
+  },
+  {
+    "revision": "12557c9b108cf5e5347f8d838014b31e",
+    "url": "/React-Portfolio/static/media/surf-report.12557c9b.jpg"
+  }
+]);
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,10 +6,6 @@ function Header(props) {
   return (
     <header className="flex-row space-between px-1">
       <h1>Robert Augusta</h1>
-      <h2>About Me</h2>
-      <h3>Portfolio</h3>
-      <h4>Contact</h4>
-      <h5>Resume</h5>
       <img src={coverImage} alt="wooden background"></img>
       {props.children}
     </header>
